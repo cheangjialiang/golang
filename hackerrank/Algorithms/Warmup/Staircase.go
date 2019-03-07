4394ef58f0dcafad6e5b59ae2e627255690d7cbd
Cheang Jia Liang (Leo) <47512872+cheangjialiang@users.noreply.github.com>
2019-03-07 11:33:34 +0800

    Create Staircase.go

---
+++ hackerrank/Algorithms/Warmup/Staircase.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main(){
+    var n int
+    fmt.Scan(&n)
+    
+    for i:=0;i<n;i++{
+        for j:=0;j<n-i-1;j++{
+            fmt.Print(" ")
+        }
+        for j:=0;j<=i;j++{
+            fmt.Print("#")
+        }
+        fmt.Println()
+    }
+}
